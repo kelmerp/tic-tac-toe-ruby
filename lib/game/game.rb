d8ef2fdc0b500167680f1219e8531a2d6a86795a
Kelmer Perez <kelmerp@gmail.com>
2013-12-31 19:04:34 -0600

    refactored #winner? into two smaller: #winner? and #get_winner, as well as removed @winner reader

--- lib/game/game.rb
+++ lib/game/game.rb
@@ -3,15 +3,24 @@ class Game
   PLAYERS = [:computer, :human]
 
   attr_reader :first_player, :computer_mark, :human_mark
-  attr_accessor :board, :winner, :current_player
+  attr_accessor :board, :current_player
 
   def initialize
     @board = ["","","","","","","","",""]
+    @winning_lines = get_winning_lines
     @first_player = get_random_player
     @current_player = @first_player
     @computer_mark = get_computer_mark
     @human_mark = get_human_mark
     @winner = false
+  end
+
+  def get_winning_lines
+    rows = [[0,1,2],[3,4,5],[6,7,8]]
+    columns = [[0,3,6],[1,4,7],[2,5,8]]
+    diagonals = [[0,4,8],[2,4,6]]
+
+    rows + columns + diagonals
   end
 
   def get_random_player
@@ -55,27 +64,48 @@ class Game
     winner? || !board.include?("")
   end
 
+  # def winner?
+  #   winner = ""
+  #   if (board[0] == board[1] && board[1] == board[2] && board[0] != "") ||
+  #     (board[0] == board[4] && board[4] == board[8] && board[0] != "") ||
+  #     (board[0] == board[3] && board[3] == board[6] && board[0] != "")
+  #       winner = board[0]
+  #   elsif (board[1] == board[4] && board[4] == board[7] && board[4] != "") ||
+  #     (board[2] == board[4] && board[4] == board[6] && board[4] != "") ||
+  #     (board[3] == board[4] && board[4] == board[5] && board[4] != "")
+  #       winner = board[4]
+  #   elsif (board[6] == board[7] && board[7] == board[8] && board[8] != "") ||
+  #     (board[2] == board[5] && board[5] == board[8] && board[8] != "")
+  #       winner = board[8]
+  #   else #no winner
+  #     return false
+  #   end
+
+  #   if winner == "x"
+  #     winner = first_player
+  #   else
+  #     winner = (PLAYERS - [first_player]).pop
+  #   end
+  # end
+
   def winner?
-    winner = ""
-    if (board[0] == board[1] && board[1] == board[2] && board[0] != "") ||
-      (board[0] == board[4] && board[4] == board[8] && board[0] != "") ||
-      (board[0] == board[3] && board[3] == board[6] && board[0] != "")
-        winner = board[0]
-    elsif (board[1] == board[4] && board[4] == board[7] && board[4] != "") ||
-      (board[2] == board[4] && board[4] == board[6] && board[4] != "") ||
-      (board[3] == board[4] && board[4] == board[5] && board[4] != "")
-        winner = board[4]
-    elsif (board[6] == board[7] && board[7] == board[8] && board[8] != "") ||
-      (board[2] == board[5] && board[5] == board[8] && board[8] != "")
-        winner = board[8]
-    else #no winner
-      return false
+    @winning_lines.each do |a, b, c|
+      return true if board[a] == board[b] && board[b] == board[c] && board[a] != ""
+    end
+    false
+  end
+
+  def get_winner
+    @winning_lines.each do |a, b, c|
+      if board[a] == board[b] && board[b] == board[c] && board[a] != ""
+        @winner = board[a]
+      end
     end
 
-    if winner == "x"
-      winner = first_player
+    if @winner == "x"
+      @winner = first_player
     else
-      winner = (PLAYERS - [first_player]).pop
+      @winner = (PLAYERS - [first_player]).pop
     end
   end
 
@@ -325,7 +355,8 @@ class Game
   end
 
   def show_winner
-    if winner
+    get_winner
+    if @winner
       puts "#{winner} is the winner"
     else
       puts "The game is a tie"
