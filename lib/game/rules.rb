b53851b596afcde52aff0ccdef7806587e280bb7
Kelmer Perez <kelmerp@gmail.com>
2014-01-08 14:55:57 -0600

    created a Rules class and moved some of the logic from Game to Rules

---
+++ lib/game/rules.rb
@@ -0,0 +1,41 @@
+require_relative 'board'
+
+class Rules
+  attr_reader :board, :num_needed_to_win
+  
+  def initialize(args = {})
+    @board = Board.new(:board_size => args[:board_size])
+    @num_needed_to_win = @board.size
+    @winning_lines = get_winning_lines
+  end
+
+  def get_winning_lines
+    board.get_lines
+  end
+  
+  def winner?
+    ["x","o"].each do |mark|
+      @winning_lines.each do |line|
+        counter = 0
+        line.each do |position|
+          counter += 1 if board.content[position] == mark
+          return true if counter == board.size
+        end
+      end
+    end
+
+    false
+  end
+
+  def get_winner
+    ["x","o"].each do |mark|
+      @winning_lines.each do |line|
+        counter = 0
+        line.each do |position|
+          counter += 1 if board.content[position] == mark
+          return mark if counter == board.size
+        end
+      end
+    end
+  end
+end
