0b852e11de0351c2e569818b1e8d5ea35b8e1cbe
Kelmer Perez <kelmerp@gmail.com>
2014-01-10 14:20:10 -0600

    added spec for Rules#get_winning_lines

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -6,6 +6,12 @@ describe Rules do
     @rules = Rules.new(:board => @board)
   end
 
+  describe "#get_winning_lines" do
+    it "should return an array of winning lines with 8 'lines'" do
+      expect(@rules.get_winning_lines.size).to eq 8
+    end
+  end
+
   describe "#winner?" do
     it "should return true if there is a winner" do
       @board.content = ["o","","x","o","x","","o","x",""]
