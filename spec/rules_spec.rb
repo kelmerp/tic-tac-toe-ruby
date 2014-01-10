ecb6f93e027ac75d62492489cb0b717c01336959
Kelmer Perez <kelmerp@gmail.com>
2014-01-10 14:25:58 -0600

    rearranged the specs so that they're in the same order as the code they are testing

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -22,4 +22,27 @@ describe Rules do
       expect(@rules.winner?).to be_false
     end
   end
+
+  describe "#get_winner" do
+    context "when 'x' is the winner" do
+      it "should return 'x'" do
+        @board.content = ["x","x","x","","","","","",""]
+        expect(@rules.get_winner).to eq "x"
+      end
+    end
+
+    context "when 'o' is the winner" do
+      it "should return 'x'" do
+        @board.content = ["o","o","o","","","","","",""]
+        expect(@rules.get_winner).to eq "o"
+      end
+    end
+
+    context "when there is no winner" do
+      it "should return nil" do
+        @board.content = ["","","","","","","","",""]
+        expect(@rules.get_winner).to be_nil
+      end
+    end
+  end
 end
