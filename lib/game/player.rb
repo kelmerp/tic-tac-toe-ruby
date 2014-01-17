12f38fbb33bb1f1eb0f9399f04b9c4aabcdc714e
Kelmer Perez <kelmerp@gmail.com>
2014-01-17 10:38:35 -0600

    created a ComputerPlayer subtype of Player that keeps track of its own AI level

--- lib/game/player.rb
+++ lib/game/player.rb
@@ -1,10 +1,12 @@
+require_relative 'ai'
+
 class Player
   attr_reader :type
 
-  def self.generate_players
+  def self.generate_players(args = {})
     players = []
 
-    players << Player.new(:type => :computer)
+    players << ComputerPlayer.new(:type => :computer, :ai_level => args[:ai_level] )
     players << Player.new(:type => :human)
 
     players
@@ -12,5 +14,14 @@ class Player
 
   def initialize(args = {})
     @type = args[:type]
+  end
+end
+
+class ComputerPlayer < Player
+  attr_reader :ai
+
+  def initialize(args = {})
+    @type = args[:type]
+    @ai = AI.generate(args[:ai_level])
   end
 end
