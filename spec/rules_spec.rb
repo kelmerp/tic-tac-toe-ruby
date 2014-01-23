8d84d17ebc9922a5f245c8b0572ba2b44eef83c8
Kelmer Perez <kelmerp@gmail.com>
2014-01-23 09:37:46 -0600

    fixed Rules spec bugs introduced by refactoring Player into its own class

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -4,6 +4,8 @@ describe Rules do
   before(:each) do
     @board = Board.new
     @rules = Rules.new(:board => @board)
+    @player1 = double(:mark => "x")
+    @player2 = double(:mark => "o")
   end
 
   describe "#get_winning_lines" do
@@ -15,11 +17,11 @@ describe Rules do
   describe "#winner?" do
     it "should return true if there is a winner" do
       @board.content = ["o","","x","o","x","","o","x",""]
-      expect(@rules.winner?).to be_true
+      expect(@rules.winner?(@player1,@player2)).to be_true
     end
 
     it "should return false if there is no winner" do
-      expect(@rules.winner?).to be_false
+      expect(@rules.winner?(@player1, @player2)).to be_false
     end
   end
 
@@ -27,21 +29,21 @@ describe Rules do
     context "when 'x' is the winner" do
       it "should return 'x'" do
         @board.content = ["x","x","x","","","","","",""]
-        expect(@rules.get_winner).to eq "x"
+        expect(@rules.get_winner(@player1, @player2)).to eq "x"
       end
     end
 
     context "when 'o' is the winner" do
       it "should return 'x'" do
         @board.content = ["o","o","o","","","","","",""]
-        expect(@rules.get_winner).to eq "o"
+        expect(@rules.get_winner(@player1, @player2)).to eq "o"
       end
     end
 
     context "when there is no winner" do
       it "should return nil" do
         @board.content = ["","","","","","","","",""]
-        expect(@rules.get_winner).to be_nil
+        expect(@rules.get_winner(@player1, @player2)).to be_nil
       end
     end
   end
